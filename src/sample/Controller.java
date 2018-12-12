d8826c0f4864461ef9243bcafc305987bf6fa15f
BT_1N3_26 <s>
2018-12-12 10:57:30 -0500

    f

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 
 public class Controller {
     public Label helloWorld;
+
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("No you're not");
+
     }
 }

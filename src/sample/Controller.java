37bd82af8bc63947750b6ddf217166361d4b6d1a
BT_1N3_26@bktech.local <BT_1N3_26@bktech.local>
2018-12-12 10:45:13 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public Label helloWorld;
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("No you're not");
+    }
+}
